f3ad8f78cc955e8b78d2c1e1391dc812d4a9615b
Jed Mitchener <jed.mitchener@gmail.com>
2016-01-15 01:13:26 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  
+  #define some useful constants...
+  sp = " "
+  p = "|"
+  dashes = "-----------"
+  
+  #now print the board
+  puts sp + board[0] + sp + p + sp + board[1] + sp + p + sp + board[2] + sp
+  puts dashes
+  puts sp + board[3] + sp + p + sp + board[4] + sp + p + sp + board[5] + sp
+  puts dashes
+  puts sp + board[6] + sp + p + sp + board[7] + sp + p + sp + board[8] + sp
+end 
